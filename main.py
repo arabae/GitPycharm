858c0178c958e176bbeb1b24b3a139afd89a1c81
arabae <46676700+arabae@users.noreply.github.com>
2020-09-25 12:04:09 +0900

    Update on GitHub

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #
 #
 #
-print('Hello, world!')
+print('Hello World!')
 
 print("Hello WOrld 2")
 
 print("Hello Wrold 3")
 
-print('Hello, world 4')
+print('Hello World 4')
 
-print('Hello, world 5')
+print('Hello World 5')
 
+print('Hello World Updates on Github')
